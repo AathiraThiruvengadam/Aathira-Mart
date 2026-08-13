bd5e5f2243b43dfcd3bac73dc456d66409c88829
AathiraThiruvengadam <aathiraravi2008@gmail.com>
2026-08-13 19:21:47 +0530

    Add files via upload

---
+++ AathiraMartApplication.java
@@ -0,0 +1,12 @@
+package com.aathiramart.aathira_mart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AathiraMartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AathiraMartApplication.class, args);
+    }
+}
---
+++ AathiraMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aathiramart.aathira_mart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AathiraMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
